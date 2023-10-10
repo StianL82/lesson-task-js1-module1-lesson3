e0a94360b397ac908ced648f49ecc13f5af0b266
StianL82 <stian.lilleng@gmail.com>
2023-10-10 14:24:44 +0200

    Update products.js

--- js/products.js
+++ js/products.js
@@ -14,3 +14,28 @@ const products = [
         soldOut: true,
     },
 ];
+
+const container = document.querySelector(".container");
+
+/* console.log(container); */
+
+let html = "";
+
+for (let i =0; i < products.length; i++){
+
+    let displayPrice = "Not Defined"
+
+    if (products[i].price) {
+        displayPrice = products[i].price;
+    }
+
+    html += `<div class="container">
+            <h4>${products[i].name}</h4>
+            <b>${displayPrice}</b>
+            <b>${products[i].soldOut}</b>
+            </div>
+`;
+console.log(html);
+}
+
+container.innerHTML = html;
